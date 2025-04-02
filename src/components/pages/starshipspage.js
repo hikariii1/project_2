e8c723b9b700eb96b0327bae8de6b16d7eac4ca3
hikariii1 <f.kuanova1@com>
2025-04-02 10:10:31 +0500

    routing

--- src/components/pages/starshipspage.js
+++ src/components/pages/starshipspage.js
@@ -1,24 +1,11 @@
-import React, {Component} from "react"
- import Row from "../row"
+import React from "react"
  import { StarshipList } from "../sw-components"
- import StarshipDetails from "../sw-components/starship-details"
+ import { withRouter } from 'react-router-dom' 
+
+ const StarshipsPage = ({ history }) => {
+    return (
+        <StarshipList onItemSelected={(itemId) => history.push(itemId)} />
+    )
+}
  
- export default class PeoplePage extends Component {
-     state = {
-         selectedItem: null
-     }
- 
-     onItemSelected = (selectedItem) => {
-         this.setState({selectedItem})
-     }
- 
-     render() {
-         const { selectedItem } = this.state
-         return (
-             <Row
-                 left={<StarshipList onItemSelected={this.onItemSelected}/>}
-                 right={<StarshipDetails itemId={selectedItem} />}
-             />
-         )
-     }
- }
+export default withRouter(StarshipsPage)
